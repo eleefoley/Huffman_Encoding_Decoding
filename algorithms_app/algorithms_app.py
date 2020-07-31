988507f60613833004dea59c5d0914bb087bb438
Emma <eleefoley@gmail.com>
2020-07-31 11:25:22 -0400

    included test for select, which needs changes

--- algorithms_app/algorithms_app.py
+++ algorithms_app/algorithms_app.py
@@ -1,11 +1,12 @@
 import math
 import random
 # used for testing the median finding function
-import statistics
+from statistics import median
 
 def main ():
 	prompt()
 
+# input from the user to determine which function(s) to go through
 def prompt():
 	print("Welcome to the CS 430 summer project!")
 	done = False
@@ -19,6 +20,7 @@ def prompt():
 			exit()
 			done = True
 		elif(response == 'a'):
+			run_tests()
 			done = True
 		elif(response == 'b'):
 			done = True
@@ -41,4 +43,41 @@ def select(S,k):
 	elif len(s_minus) >= k:
 		select(s_minus,k)
 
+##########################functions used for testing#############3
+# given a lenght, generate a random 
+def random_unsorted_list(n):
+	test_list = []
+	for i in range(n):
+		test_list.append(random.choice(range(1,100)))
+	return test_list
+
+
+# test the select function for randomized median finding
+def test_select():
+	print("testing select function for randomized median finding")
+	for i in range(3,8,2):
+		unsorted_list = random_unsorted_list(i)
+		unsorted_list = [83, 53, 38, 44, 51, 52]
+		if len(unsorted_list) % 2 != 0:
+    			k = len(unsorted_list)/2 + 1
+		else:
+    			k = len(unsorted_list) / 2 + .5
+			print(k)
+		my_median = select(unsorted_list, k)
+		true_median = median(unsorted_list)
+		if my_median != true_median:
+			print("	select function failed to find the median for array " + str(unsorted_list) + "\n")
+			print(str(k) + " != " + str(true_median))
+			return False
+	print("	select function successfully finds the median")
+	return True
+		
+# run all the tests, returning True if they pass, Fail if they do not
+def run_tests():
+	print("Running tests")
+	select = test_select()
+	if(select == False):
+		print("	Failed one or more tests")
+	else:
+		print("	All tests passed")
 main()
