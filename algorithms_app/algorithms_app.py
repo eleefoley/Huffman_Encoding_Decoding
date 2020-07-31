fcc41ac08d872cfe1dc43759d87d93bf6ad054f4
Emma <eleefoley@gmail.com>
2020-07-31 10:13:55 -0400

    median finding in the notebook, creating user prompt

---
+++ algorithms_app/algorithms_app.py
@@ -0,0 +1,26 @@
+import math
+import random
+# used for testing the median finding function
+import statistics
+
+def main ():
+
+
+def prompt():
+	print("Welcome to the CS 430 summer project!")
+	while(done == False):
+		response = input("""Type the letter for what you would like to do:
+			a:  Run in test mode
+			b:  Choose a function to run
+			c:  Exit the program
+		""")
+	if(response == 'c'):
+		exit()
+		done = True
+	elif(response == 'a'):
+		done = True
+	elif response == 'b'):
+		done = True
+	else(print("Try again\n")
+
+main()
