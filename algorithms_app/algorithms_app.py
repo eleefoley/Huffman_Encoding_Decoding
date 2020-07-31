d26a3615cd8a3d224433ed18ff4b7c27e946786d
Emma <eleefoley@gmail.com>
2020-07-31 10:21:26 -0400

    initial user prompt works correctly

--- algorithms_app/algorithms_app.py
+++ algorithms_app/algorithms_app.py
@@ -4,23 +4,25 @@ import random
 import statistics
 
 def main ():
-
+	prompt()
 
 def prompt():
 	print("Welcome to the CS 430 summer project!")
+	done = False
 	while(done == False):
 		response = input("""Type the letter for what you would like to do:
 			a:  Run in test mode
 			b:  Choose a function to run
 			c:  Exit the program
 		""")
-	if(response == 'c'):
-		exit()
-		done = True
-	elif(response == 'a'):
-		done = True
-	elif response == 'b'):
-		done = True
-	else(print("Try again\n")
+		if(response == 'c'):
+			exit()
+			done = True
+		elif(response == 'a'):
+			done = True
+		elif(response == 'b'):
+			done = True
+		else:
+			print("Try again\n")
 
 main()
