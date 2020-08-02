060e9b9a92f380701cf6eea5be6778371f860ccc
Emma <eleefoley@gmail.com>
2020-08-02 11:42:30 -0400

    functions for reading in file and counting characters

--- algorithms_app/huffman.py
+++ algorithms_app/huffman.py
@@ -1,11 +1,25 @@
-import pandas
+import pandas as pd
 from pathlib import Path
 
-file_name = 'pride_and_prejudice_ch_1.txt'
-#folder_path = Path("./text/")
-file_to_open = Path("text/" + file_name)
-print(file_to_open)
-print(file_to_open.exists())
+def main():
+	data = textfile_to_str()
+	count_df = str_to_count_df(data)
+	
+def textfile_to_str():
+	file_name = 'pride_and_prejudice_ch_1.txt'
+	file_to_open = Path("text/" + file_name)
+	print("Opening the file to read in characters")
+	
+	data = str()
+	with open(str(file_to_open), 'r') as file:
+    		data = file.read().replace('\n', '')
+	return(data)
 
-with open(file_to_open, 'r') as file:
-    data = file.read().replace('\n', '')
+def str_to_count_df(text_string):
+	char_df = pd.DataFrame(list(text_string), columns = ['char'])
+	grouped_df = char_df.groupby('char').size().reset_index(name = "count").sort_values(by = ['count', 'char'], ascending = False)
+	print("Here are the most frequent characters in the file:\n")
+	print(grouped_df.head())
+	return grouped_df
+
+main()
